64cc8dad1f43b028e5dbc5d7dcc479a56cab645f
MRanzato <matteo.ranzato@studenti.unipd.it>
2018-11-10 12:49:45 +0100

    Aggiunto div overlay

--- JS/custom.js
+++ JS/custom.js
@@ -1,9 +1,11 @@
 window.onload = load;
 function load() {
 	document.getElementById("burger-menu").addEventListener("touchend", click);
+	var overlay = document.querySelector("#content .overlay");
 
 	function click(){
 		var el = document.getElementById("sidebar-wrapper");
 		el.classList.toggle("toggled");
+		overlay.style.display = overlay.style.display == "block" ? "none" : "block";
 	}
 }
