02113537382c7038ff4cca28d95b6639ff431dbf
LauraCameran <lauracameran@gmail.com>
2019-02-10 19:03:01 +0100

    Ora non dà errore lasciare un commento

--- PHP/inseriscicommento.php
+++ PHP/inseriscicommento.php
@@ -10,8 +10,9 @@ $user = init();
 $validField = preg_match('/^.*[a-zA-Z].*$/',$_POST['content']);
 
 if ($user->isRegistered() && $_SESSION['ID'] == $user->getID()){
-	if ($validField)
-		$user->insertComment($_POST['articleID'],$_POST['content']);
+	if ($validField) {
+        $user->insertComment($_POST['articleID'], $_POST['content']);
+    }
 	else
 		$_SESSION['commenterror'] = true;
 }
