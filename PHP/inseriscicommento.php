26244446ac25168fcf5595e010568fa75e71671b
benny1693 <benedetto.cosentino@studenti.unipd.it>
2019-02-10 22:39:45 +0100

    Sistemazione stile inseriscicommento.php

--- PHP/inseriscicommento.php
+++ PHP/inseriscicommento.php
@@ -10,9 +10,8 @@ $user = init();
 $validField = preg_match('/^(.*[a-zA-Z].*\r*\n*)(.*[a-zA-Z]*.*\r*\n*)*$/',$_POST['content']);
 
 if ($user->isRegistered() && $_SESSION['ID'] == $user->getID()){
-	if ($validField) {
-        $user->insertComment($_POST['articleID'], $_POST['content']);
-    }
+	if ($validField)
+		$user->insertComment($_POST['articleID'], $_POST['content']);
 	else
 		$_SESSION['commenterror'] = true;
 }
