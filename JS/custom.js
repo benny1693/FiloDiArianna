2aab51a42be46ee628d25b359d36eee642b6584b
benny1693 <benedetto.cosentino@studenti.unipd.it>
2019-02-14 11:07:54 +0100

    Correzioni

--- JS/custom.js
+++ JS/custom.js
@@ -88,34 +88,6 @@ function isPasswordEqual(){
 	return showAlert(alert, password.value === confermaPassword.value, 'Le password non combaciano', parent);
 }
 
-// FIXME: eliminare la seguente funzione
-function validateForm(){
-	var name = document.getElementById("inputName");
-	var surname = document.getElementById("inputSurname");
-	var email = document.getElementById("inputEmail");
-	var username = document.getElementById("inputUsername");
-	var password = document.getElementById("inputPassword");
-	var confermaPassword = document.getElementById("inputPasswordConfirm");
-
-	var regExp = /^[a-zA-Z]{1,20}$/;
-	var regexpUsername = /^[a-zA-Z0-9]+$/;
-	var regExpPassword = /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[!@#$%^&*])[\w!@#$%^&*]{6,12}$/;
-	// espressione regolare in accordo con lo standard RFC 822
-	var regExpEmail = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
-
-	var boolName = checkText(name, "Nome non valido", regExp);
-	var boolSurname = checkText(surname, "Cognome non valido", regExp);
-	var boolEmail = checkText(email, "Email non valida", regExpEmail);
-	var boolUsername = checkText(username, "Username non valida", regexpUsername);
-	var boolPassword = checkText(password, "Password non corretta", regExpPassword);
-	var boolConfermaPassword = isPasswordEqual(password, confermaPassword, "Le password non combaciano");
-
-	var button = document.getElementById("fatto");
-
-	return boolName && boolSurname && boolEmail && boolUsername && boolPassword && boolConfermaPassword;
-}
-
-
 function AlertFilesize(){
 	var filepath = document.getElementById("FormControlFile").value;
 
